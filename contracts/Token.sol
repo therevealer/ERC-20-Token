6a4faf3d843a1823df345030b7a771858a091454
Aguchukwu Emmanuel <therevealerdev@gmail.com>
2023-05-22 06:53:39 +0100

    Added some modifcations to the ERC20 Token"
    git commit -m Adde

--- contracts/Token.sol
+++ contracts/Token.sol
@@ -5,7 +5,20 @@ pragma solidity ^0.8.9;
 	import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
 contract Token is ERC20 {
+    uint price = 0.01 ether;
+
     constructor() ERC20 ("Nodle Token", "NODL") {
         _mint(msg.sender, 1_000_000 * 10 * decimals());
+    }
+
+    // Users can burn their Tokens
+    function burn (uint256 amount) external {
+        _burn(msg.sender, amount);
+    }
+
+    //Users can mint new tokens by paying ether
+    function buy () external payable {
+        require(msg.value > 0, "You have to send some Ether");
+        _mint(msg.sender, msg.value * 10 ** decimals() / price);
     }
 }
