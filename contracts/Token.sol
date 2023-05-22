af622a8e75b4cc5aa8972eb686e66de685760a0c
Aguchukwu Emmanuel <therevealerdev@gmail.com>
2023-05-22 06:14:27 +0100

    Started Writing Codebase

---
+++ contracts/Token.sol
@@ -0,0 +1,9 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.9;
+
+// import "hardhat/console.sol";
+	import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+
+contract Token is ERC20 {
+    constructor()
+}
