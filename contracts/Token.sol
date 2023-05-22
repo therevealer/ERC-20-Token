7c8872a1adbe295c9b97011f290940747f23045e
Aguchukwu Emmanuel <therevealerdev@gmail.com>
2023-05-22 06:22:17 +0100

    Completed Token basic functionalities

--- contracts/Token.sol
+++ contracts/Token.sol
@@ -5,5 +5,7 @@ pragma solidity ^0.8.9;
 	import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
 contract Token is ERC20 {
-    constructor()
+    constructor() ERC20 ("Nodle Token", "NODL") {
+        _mint(msg.sender, 1_000_000 * 10 * decimals());
+    }
 }
